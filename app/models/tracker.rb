b2c0e76ec59c95a6030326eb9313c71c799b692f
kjplank <plank@KJs-MacBook-Air.local>
2015-06-09 19:04:38 -0500

    Working on validations

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,8 +1,12 @@
 class Tracker < ActiveRecord::Base
-validates :tag_id, :presence => true
 validates :user_id, :presence => true
-validates :relationship_id, :presence => true
 validates :user_id, :uniqueness => {:scope => [:relationship_id, :tag_id] }
+
+# validates :tag_id, :presence => true
+# validates :relationship_id, :presence => true
+
+validates :tag_id, :presence => true, if: "relationship_id.nil?"
+validates :relationship_id, :presence => true, if: "user_id.nil?"
 
 belongs_to :relationship
 belongs_to :tag
